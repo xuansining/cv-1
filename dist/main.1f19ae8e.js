14abe5747c003819582fe2b9101a23d00d84ea33
xuansining <1357821160@qq.com>
2020-08-07 11:05:49 +0800

    update bulid

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -175,7 +175,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58954" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51718" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -352,4 +352,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=/main.1f19ae8e.js.map
+//# sourceMappingURL=main.1f19ae8e.js.map
--- dist/style.e308ff8e.js
+++ dist/style.e308ff8e.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58954" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51718" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../../AppData/Local/Yarn/Data/global/node_modules/parcel/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/style.e308ff8e.js.map
+//# sourceMappingURL=style.e308ff8e.js.map
